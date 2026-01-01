980bd258defb8b52f94cf80088133126f7d9d264
Anuja <anuja3327@gmail.com>
2026-01-01 12:46:43 +0530

    renamed

--- CHAPTER-12/01_walrus.py
+++ CHAPTER-12/01_walrus.py
@@ -1,5 +1,5 @@
 #using walrus operator 
-# assignmnet and operation in one line
+# assignment and operation in 1 line
 if(n:= len([1,2,3,4,5])) >3:
     print(f"list is long {n} , expected <=3)")
     
